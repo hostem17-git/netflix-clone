f07482c1066942f73483bea86c21300fe35d6afa
hostem17 <vjjais17@gmail.com>
2021-08-01 21:39:43 +0530

    navbar done:

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import "./Nav.css"
+
+
+function Nav() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
+    return (
+        <div className={`navbar ${scrolled && "navbar-black"}`} >
+            <img className="navbar-logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix-logo" />
+            <img className="navbar-avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
+        </div>
+    )
+}
+export default Nav
